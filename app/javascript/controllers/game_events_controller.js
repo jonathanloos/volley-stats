05fcf414bfefda5189610aafba09b04c357a5769
Jonathan Loos <jonathan.loos@ecn.forces.gc.ca>
2023-07-22 00:42:50 +0000

    feat: clear button

--- app/javascript/controllers/game_events_controller.js
+++ app/javascript/controllers/game_events_controller.js
@@ -1,24 +1,45 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["actionButton", "player", "playerSelection", "actionSelection", "qualitySelection"]
+  static targets = ["actionButton", "player", "playerSelection", "actionSelection", "qualitySelection", "clear", "clearButton"]
 
   connect() { }
 
+  clear () {
+    // reset all action buttons
+    this.toggleActionButtons()
+
+    // reset all player buttons
+    this.togglePlayers()
+
+    // clear the play outline
+    this.clearPlayOutline()
+
+    // hide the clear button
+    this.clearButtonTarget.classList.add('d-none')
+  }
+
   selectActionEvent(event) {
-    // Disable the rest of the rally buttons
-    this.disableActionButtons(event.target)
+    // Disable the rest of the action buttons
+    this.toggleActionButtons(event.target)
 
     // Add action to overview sentence
     this.actionSelectionTarget.innerHTML = `${event.target.dataset.actionName} (${event.target.dataset.actionType}) `
 
     // Disable inapplicable quality sections
+
+    // Show the clear button
+    this.clearButtonTarget.classList.remove('d-none')
   }
 
   // loop through all buttons and disable them except the activeButton
-  disableActionButtons(activeButton) {
+  // if no action button is passed in enable all buttons
+  toggleActionButtons(activeButton) {
     this.actionButtonTargets.forEach(button => {
-      if (button !== activeButton) {
+      if (activeButton === undefined) {
+        button.classList.remove("disabled")
+
+      } else if (button !== activeButton) {
         button.classList.add("disabled")
       }
     })
@@ -28,7 +49,7 @@ export default class extends Controller {
     let activePlayer = event.target
 
     // Weird HTML thing if they click the small tag instead of the li
-    if (event.target.dataset.name === undefined) {
+    if (activePlayer.dataset.name === undefined) {
       activePlayer = event.target.parentElement
     }
 
@@ -36,17 +57,29 @@ export default class extends Controller {
     activePlayer.classList.add("active")
 
     // Disable the rest of the players
-    this.disablePlayers(activePlayer)
+    this.togglePlayers(activePlayer)
 
     // Add players name to play outline
     this.playerSelectionTarget.innerHTML = activePlayer.dataset.name
+
+    // Show the clear button
+    this.clearButtonTarget.classList.remove('d-none')
   }
 
-  disablePlayers(activePlayer) {
+  togglePlayers(activePlayer) {
     this.playerTargets.forEach(button => {
-      if (button !== activePlayer) {
+      if (activePlayer === undefined) {
+        button.classList.remove("disabled")
+        button.classList.remove("active")
+
+      } else if (button !== activePlayer) {
         button.classList.add("disabled")
       }
     })
+  }
+
+  clearPlayOutline () {
+    this.playerSelectionTarget.innerHTML = null
+    this.actionSelectionTarget.innerHTML = null
   }
 }
