12caee9f517db18bc9cafcdcdcf65bd0b169d49c
Jonathan Loos <jonathan.loos@ecn.forces.gc.ca>
2023-07-23 13:05:10 +0000

    feat: db dx

--- launch.sh
+++ launch.sh
@@ -2,10 +2,18 @@ function help () {
   echo "commands:"
   echo
   echo "  go - start the rails server and bind host & ports"
+  echo "  db - reset the database with seed data"
 }
 
 function go () {
   bundle exec rails s --binding 0.0.0.0 --port 3000
+}
+
+function db () {
+  bundle exec rails db:drop
+  bundle exec rails db:create
+  bundle exec rails db:migrate
+  bundle exec rails db:seed
 }
 
 help
