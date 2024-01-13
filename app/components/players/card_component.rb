dd2d57e6a5d595e13cc99e3cae3cf72ff067e8b3
Jonathan Loos <jonathanloos97@gmail.com>
2024-01-13 23:13:11 +0000

    fix: away team points

--- app/components/players/card_component.rb
+++ app/components/players/card_component.rb
@@ -4,5 +4,10 @@ class Players::CardComponent < ApplicationComponent
   def initialize(player:)
     @player = player
     @volleyball_set = @player.volleyball_set
+    @most_recent_event = if @player.persisted?
+      @player.events.last
+    else
+      @volleyball_set.events.where(team: @volleyball_set.game.away_team).last
+    end
   end
 end
