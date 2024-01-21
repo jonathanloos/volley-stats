a6af8312e1929a400f6a1e5b0df6402c93058753
Jonathan Loos <jonathanloos97@gmail.com>
2024-01-21 02:39:48 +0000

    fix: scope player to volleyball set

--- app/components/volleyball_sets/timeout_component.rb
+++ app/components/volleyball_sets/timeout_component.rb
@@ -6,10 +6,10 @@ class VolleyballSets::TimeoutComponent < ApplicationComponent
     @home = home
 
     if @home
-      @player = @volleyball_set.players.unscope(:where, :order).find_by(user: @volleyball_set.game.home_team.head_coach)
+      @player = Player.find_by(volleyball_set: @volleyball_set, user: @volleyball_set.game.home_team.head_coach)
       @timeouts_left = @volleyball_set.home_time_outs_left
     else
-      @player = @volleyball_set.players.unscope(:where, :order).find_by(team: @volleyball_set.game.away_team)
+      @player = Player.find_by(volleyball_set: @volleyball_set, team: @volleyball_set.game.away_team)
       @timeouts_left = @volleyball_set.away_time_outs_left
     end
 
