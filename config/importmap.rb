1dced0b07be1d9e47d2194129b6894a9b6201bc1
Jonathan Loos <jonathan.loos@ecn.forces.gc.ca>
2024-02-16 23:36:18 +0000

    chore: undo sidecar

--- config/importmap.rb
+++ config/importmap.rb
@@ -11,5 +11,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
-
-pin_all_from "app/components", under: "components"
