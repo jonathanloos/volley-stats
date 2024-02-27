a363b2a30d65967774f32590cd5ab680a8d35dbe
Jonathan Loos <jonathan.loos@ecn.forces.gc.ca>
2024-02-27 01:11:41 +0000

    feat: default starting rotation to current rotation

--- db/migrate/20240225163157_add_starting_rotation_to_players.rb
+++ db/migrate/20240225163157_add_starting_rotation_to_players.rb
@@ -2,5 +2,7 @@ class AddStartingRotationToPlayers < ActiveRecord::Migration[7.1]
   def change
     add_column :players, :starting_rotation, :integer
     add_column :players, :starting_libero, :boolean
+
+    Player.update_all("starting_rotation = rotation")
   end
 end
